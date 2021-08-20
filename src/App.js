fd857ecbf611013addc80ae3648ede554685edef
Harshit Saini <harshitsaini3582@gmail.com>
2021-08-20 21:44:19 +0530

    new changes to sign up page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from '../src/components/pages/homepage/homepage';
-import {Route,Switch} from 'react-router-dom';
+import {Route,Switch,Redirect} from 'react-router-dom';
 import ShopPage from '../src/components/pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './components/sign-in-and-sign-out/sign-in-sign-out';
@@ -42,16 +42,22 @@ class App extends React.Component{
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path='/signin' component={SignInAndSignUpPage} />
+            <Route exact path='/signin' render={() => this.props.currentUser ?
+            (<Redirect to='/'/>):(<SignInAndSignUpPage/>)
+             } />
           </Switch>
         </div>
       );
     }
 }
 
+const mapStateToProps = ({user})=>({
+   currentUser:user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser:user => dispatch(setCurrentUser(user))
 })
 
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
